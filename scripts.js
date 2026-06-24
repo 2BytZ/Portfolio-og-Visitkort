410008a12ddad7333b38fd97a96c3d7fe8f32511
Tobias R. <no@thankyou.com>
2026-06-24 09:42:30 +0200

    added php and js code to project

--- scripts.js
+++ scripts.js
@@ -1,3 +1,50 @@
+// Create an event listener to get data from form when submit button is pressed
 document.querySelector('#contact-form-details').addEventListener('submit',function(event) {
     event.preventDefault()
-})
+    // Save data in const from event listener
+    const form = event.currentTarget;
+    // Put form event data into list of FormData
+    const formData = new FormData(form)
+
+    // Save and trim individual data from FromData form
+    const name = formData.get('name').trim();
+    const email = formData.get('email').trim();
+    const message = formData.get('message').trim();
+    if (!name) {
+        alert('Please input your name');
+        return;    
+    }
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) {
+        alert('Please make use of a valid E-mail');
+        return;
+    }
+    if (!message) {
+        alert('Please provide a message to send')
+        return;
+    }
+
+    fetch('http://localhost:8000/send-message.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, email, message }),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Something went wrong');        
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Success: ', data);
+        alert('Message sent!');
+        form.reset(); // form.reset doesn't exist?
+    })
+    .catch(error => {
+        console.error('Error: ', error)
+        alert('An error occured. Try again later.');
+    });
+});
+
